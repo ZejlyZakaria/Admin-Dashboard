50cc618bc23c205652d50a04031b3510df55ba52
Zakaria Zejly <zejly12@gmail.com>
2022-11-11 11:05:43 +0100

    add sidebar and navbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./home/Home";
+import Login from "./login/Login";
+import List from "./list/List";
+import Single from "./single/Single";
+import New from "./new/New";
 
 function App() {
   return (
     <div className="App">
-      this is app
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element ={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element ={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
